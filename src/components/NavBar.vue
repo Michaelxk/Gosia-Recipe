a4fbd6708a575a2ce2b18dfd7941243c0705041b
Michael Linares <macbookair@Mike-Mac.local>
2020-06-06 13:05:35 -0400

    adding hover to the brand

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <div class="navbar-content">
     <nav class="navbar navbar-expand-lg navbar-light bg-warning" role="navigation">
       <a class="navbar-brand" href="#" @click="goHome()">Eat-Smart</a>
-      <button
+      <!-- <button
         class="navbar-toggler"
         type="button"
         data-toggle="collapse"
@@ -12,7 +12,7 @@
         aria-label="Toggle navigation"
       >
         <span class="navbar-toggler-icon"></span>
-      </button>
+      </button>-->
       <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
           <router-link :to="{ name: 'home' }">
@@ -47,5 +47,11 @@ nav {
   -webkit-box-shadow: 2px 13px 18px -14px rgba(0, 0, 0, 1);
   -moz-box-shadow: 2px 13px 18px -14px rgba(0, 0, 0, 1);
   box-shadow: 2px 13px 18px -14px rgba(0, 0, 0, 1);
+}
+.navbar-brand {
+  font-weight: bold;
+}
+.navbar-brand:hover {
+  color: #fff;
 }
 </style>
