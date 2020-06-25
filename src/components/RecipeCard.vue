bb38fcf6262ad00dd6b85c84ea4912f933a750bc
cesarmartinez <cesarmartinezd@gmail.com>
2020-06-25 17:53:53 +0200

    recommendations

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -1,89 +1,25 @@
 <template>
-  <div class="container">
-    <div class="searchRecipe-content">
-      <form class="form-inline my-2 my-lg-0" @submit.prevent="fetchRecipes">
-        <b-form-input
-          class="form-control mr-sm-2"
-          type="search"
-          placeholder="Search Recipes"
-          v-model="input"
-          required
-        ></b-form-input>
-      </form>
-    </div>
-    <div class="result">
-      <b-card
-        v-for="(recipe, id) in recipes"
-        :key="id"
-        :img-src="baseUri + recipe.image"
-        img-alt="Image"
-        img-top
-        tag="article"
-        style="max-width: 20rem;"
-        class="img-fluid mb-2"
-      >
-        <h4>{{ recipe.title }}</h4>
-        <b-card-text>
-          <span class="badge badge-info">Servings: {{ recipe.servings }}</span>
-          <span class="badge badge-secondary mr-10"
-            >Ready in : {{ recipe.readyInMinutes }} Minutes</span
-          >
-        </b-card-text>
-        <span>
-          <router-link
-            @submit.prevent="getRecipeInformation"
-            class="btn btn-warning"
-            :to="{ name: 'nutritionFact', params: { id: recipe.id } }"
-            >Preparation</router-link
-          >
-        </span>
-      </b-card>
-    </div>
-  </div>
+  <b-card
+    v-for="(recipe, id) in recipes"
+    :key="id"
+    :img-src="baseUri + recipe.image"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="img-fluid mb-2"
+  >
+    <h4>{{ recipe.title }}</h4>
+    <b-card-text>
+      <span class="badge badge-info">Servings: {{ recipe.servings }}</span>
+      <span class="badge badge-secondary mr-10">Ready in : {{ recipe.readyInMinutes }} Minutes</span>
+    </b-card-text>
+    <span>
+      <router-link
+        @submit.prevent="getRecipeInformation"
+        class="btn btn-warning"
+        :to="{ name: 'nutritionFact', params: { id: recipe.id } }"
+      >Preparation</router-link>
+    </span>
+  </b-card>
 </template>
-<script>
-import { mapState } from 'vuex';
-
-export default {
-  name: 'RecipeCard',
-  props: ['RecipeCard'],
-  data() {
-    return {
-      input: ''
-    };
-  },
-  created() {
-    this.fetchRecipes();
-  },
-  computed: mapState(['recipes', 'baseUri']),
-  methods: {
-    fetchRecipes() {
-      this.$store.dispatch('getRecipes', this.input);
-    }
-  }
-};
-</script>
-<style scope>
-.form-inline .form-control {
-  display: flex;
-  width: 80%;
-  margin: 0 auto;
-  margin-top: -60px;
-  z-index: 1;
-}
-
-b-card-text {
-  display: flex;
-}
-
-span:nth-child(1) {
-  margin-right: 10px;
-}
-
-.img-fluid {
-  width: 90%;
-  max-width: 100%;
-  display: flex;
-  margin: 0 auto;
-}
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,56 +1,53 @@
 <template>
   <div id="app">
-    <header role="header" class="header">
-      <div class="jumbotron">
-        <div class="container-info">
-          <h2 class="title text-center">Gosia</h2>
-          <h1 class="title text-center">Recipes</h1>
-        </div>
-      </div>
-    </header>
+    <Header />
 
-    <RecipeCard></RecipeCard>
+    <Search />
+
+    <RecipeCardList :recipes="recipes"></RecipeCardList>
+
     <Footer></Footer>
   </div>
 </template>
 
 <script>
-import RecipeCard from '@/components/RecipeCard.vue';
+import RecipeCardList from '@/components/RecipeCardList.vue';
 import Footer from '@/components/Footer.vue';
 
 export default {
-  name: 'home',
-  components: { RecipeCard, Footer },
-  data() {
-    return {
-      recipes: []
-    };
-  }
+	name: 'home',
+	components: { RecipeCardList, Footer },
+	data() {
+		return {
+			recipes: [],
+			search: ''
+		};
+	}
 };
 </script>
 <style lang="scss" scoped>
 .header {
-  height: 34%;
+	height: 34%;
 }
 .jumbotron {
-  background-image: url('https://www.diabetes.org/sites/default/files/styles/paragraph_50_50/public/2019-08/Diabetes-Superfoods-min.jpg');
-  background-size: cover;
-  .title {
-    color: #000;
-    letter-spacing: 1px;
-    font-family: 'Lilita One', cursive;
-    z-index: 1;
-    margin: 0;
-    letter-spacing: 3px;
-  }
-  h1 {
-    padding-bottom: 20px;
-    font-size: 2.7em;
-  }
-  h2 {
-    font-size: 2.5em;
-    padding-top: 20px;
-  }
+	background-image: url('https://www.diabetes.org/sites/default/files/styles/paragraph_50_50/public/2019-08/Diabetes-Superfoods-min.jpg');
+	background-size: cover;
+	.title {
+		color: #000;
+		letter-spacing: 1px;
+		font-family: 'Lilita One', cursive;
+		z-index: 1;
+		margin: 0;
+		letter-spacing: 3px;
+	}
+	h1 {
+		padding-bottom: 20px;
+		font-size: 2.7em;
+	}
+	h2 {
+		font-size: 2.5em;
+		padding-top: 20px;
+	}
 }
 
 // .jumbotron:before {
